354a5319b311f744ddb2ae62dded82bb291b57cb
Adam Spinek <adamspinek@gmail.com>
2021-01-28 10:05:52 +0100

    core(game): add draft game UI

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,6 +2,14 @@
 class App {
     _init() {
         console.log('start');
+
+        const playBtn = document.querySelector(".btn-main")!;
+        const landingPage = document.querySelector<HTMLElement>(".landingPage")!;
+        const gameContainer = document.querySelector(".game-container")!;
+
+        playBtn.addEventListener('click', () => {
+            landingPage.style.display = "none";
+        })
     }
 }
 
