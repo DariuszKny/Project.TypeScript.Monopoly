34cc51ce33be00611b18d3bf5eb039a3b279aeb6
marlexxa <marlexxa@gmail.com>
2021-02-11 23:09:17 +0100

    feat: add playerPassedGO function

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -2,8 +2,20 @@ import { Player } from "../models/player.model";
 import { throwDice } from "../models/dice.model"
 
 export const movePlayer = (player: Player) => {
-  const [,,sumOfThrownDices] = throwDice(player); 
-  if(player.playerAreDiceRolled) {
-    player.playerPosition = (player.playerPosition + sumOfThrownDices) % 40; 
+  const [, , sumOfThrownDices] = throwDice(player);
+  if (player.playerAreDiceRolled) {
+    player.playerPreviousPosition = player.playerCurrentPosition;
+    player.playerCurrentPosition = (player.playerCurrentPosition + sumOfThrownDices) % 40;
+
+    if (playerPassedGO(player)) {
+      //give 200 to player
+    }
   }
 }
+
+
+const playerPassedGO = (player: Player): boolean => {
+  if (player.playerIsJailed) return false;
+  return player.playerPreviousPosition > player.playerCurrentPosition;
+}
+
