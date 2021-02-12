3ebb052f908a9cc1e5cc3c984b4d6ec95abb47b9
marlexxa <marlexxa@gmail.com>
2021-02-12 14:55:37 +0100

    feat(city-card.service): add buyHouse function

--- src/app/services/city-card.service.ts
+++ src/app/services/city-card.service.ts
@@ -2,7 +2,18 @@ import { Player } from "../models/player.model";
 import { CityCard } from "../models/city-card.model";
 
 export const payRent = (player: Player, city: CityCard) => {
-  if(player.playerCards.includes(city.id)) return; // if player own this property do nothing
+  if(playerOwnsProperty(player, city)) return; 
   const rent = city.rent[city.numberOfHouses];
-  //player.giveMoney(rent) 
+  player.giveMoney(rent);
+}
+
+export const buyHouse = (player: Player, city: CityCard) => {
+  if(playerOwnsProperty(player, city) && city.numberOfHouses < 4){
+    player.takeMoney(city.priceOfHouses);
+    city.numberOfHouses++;
+  }
+}
+
+const playerOwnsProperty = (player: Player, city: CityCard): boolean => {
+  return player.playerCards.includes(city.id)
 }
