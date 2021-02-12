986b4b1576f79d25cd1feaa30fa1711f6971969f
marlexxa <marlexxa@gmail.com>
2021-02-12 15:12:56 +0100

    refactor: change movePlayer function to moveWhenDicesRolled

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -2,11 +2,11 @@ import { Player } from "../models/player.model";
 import { throwDice } from "../models/dice.model"
 import { SALARY_FOR_PASSIG_GO } from "../models/prices"
 
-export const movePlayer = (player: Player) => {
+
+export const moveWhenDicesRolled = (player: Player) => {
   const [, , sumOfThrownDices] = throwDice(player);
   if (player.playerAreDiceRolled) {
-    player.playerPreviousPosition = player.playerCurrentPosition;
-    player.playerCurrentPosition = (player.playerCurrentPosition + sumOfThrownDices) % 40;
+    player.move(sumOfThrownDices);
 
     if (playerPassedGO(player)) {
       player.takeMoney(SALARY_FOR_PASSIG_GO); 
