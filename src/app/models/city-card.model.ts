9008afc69f5636778ca5cc987226d60d5c8cae9b
marlexxa <marlexxa@gmail.com>
2021-02-12 02:00:59 +0100

    feat: add CityCard class

---
+++ src/app/models/city-card.model.ts
@@ -0,0 +1,32 @@
+import { ObtainableCard } from "./abstract-card.model";
+import { Province } from "./provinces"
+
+export class CityCard extends ObtainableCard {
+  readonly priceOfHouses: number = 0;
+
+  constructor(
+    readonly id: number,
+    readonly name: string,
+    readonly description: string,
+    readonly price: number,
+    readonly province: Province,
+    readonly rent: number[]
+  ) {
+    super(id, name, description, price);
+    switch(province){
+      case 1: 
+      case 2:
+              this.priceOfHouses = 50;
+              break;
+      case 3: 
+      case 4: this.priceOfHouses = 100;
+              break;    
+      case 5:
+      case 6: this.priceOfHouses = 150;
+              break;
+      case 7: 
+      case 8: this.priceOfHouses = 200;
+              break;  
+    }
+  }
+}
