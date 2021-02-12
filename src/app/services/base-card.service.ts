0bc675f645e7a8be89e230508daeaee79c3e135f
marlexxa <marlexxa@gmail.com>
2021-02-12 21:07:20 +0100

    feat(base-card): add goToJail fuction

--- src/app/services/base-card.service.ts
+++ src/app/services/base-card.service.ts
@@ -1,6 +1,12 @@
 import { Player } from "../models/player.model";
+import { JAIL_FIELD } from "../constants/game-constants";
 
 export const playerPassedStart = (player: Player): boolean => {
   if (player.playerIsJailed) return false;
   return player.playerPreviousPosition > player.playerCurrentPosition;
+}
+
+export const goToJail = (player: Player) => {
+  player.playerIsJailed = true;
+  player.playerCurrentPosition = JAIL_FIELD;
 }
