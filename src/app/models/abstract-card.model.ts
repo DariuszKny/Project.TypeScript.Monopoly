043270c6f47b5c559487a239fb5237786e5c03e5
marlexxa <marlexxa@gmail.com>
2021-02-12 01:57:58 +0100

    feat: add abstract ObtainableCard class

--- src/app/models/abstract-card.model.ts
+++ src/app/models/abstract-card.model.ts
@@ -2,8 +2,27 @@ export abstract class Card {
   constructor(
     readonly id: number,
     readonly name: string,
-    readonly description: string,
-    readonly obtainable = false    
+    readonly description: string,  
   ) {}
+}
+
+export abstract class ObtainableCard extends Card {
+  private _isObtainable: boolean = true;
+  constructor(
+    readonly id: number,
+    readonly name: string,
+    readonly description: string, 
+    readonly price: number 
+  ) {
+    super(id, name, description)
+  }
+
+  get isObtainable(): boolean {
+    return this._isObtainable;
+  }
+
+  set isObtainable(isObtainable: boolean) {
+    this._isObtainable = isObtainable;
+  }
 
 }
