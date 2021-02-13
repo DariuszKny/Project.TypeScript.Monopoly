310c7299e0900da496d17e9882aed098a3846eec
DariuszKny <nasz_email_do_github>
2021-02-14 00:13:34 +0100

    feat(rightMenu.view): added rightMenu.service.ts

---
+++ src/app/views/rightMenu.view.ts
@@ -0,0 +1,5 @@
+export class RightMenuView {
+    readonly fields = document.querySelectorAll('.player');
+    container = document.querySelector(".cards-container")! as HTMLImageElement;
+    string = "RIGHT CONTAINER WORKS"
+}
