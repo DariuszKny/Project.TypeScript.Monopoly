4233c7f98f1012219ae44fa6df90665fea3f375c
marlexxa <marlexxa@gmail.com>
2021-02-13 01:15:29 +0100

    refactor(artifact-card): change parameter type of playerOwnsArtifact function

--- src/app/services/artifact-card.service.ts
+++ src/app/services/artifact-card.service.ts
@@ -4,20 +4,20 @@ import { ARTIFACT_CARDS_IDS } from "../constants/game-constants";
 
 export const payTax = (currentPlayer: Player, players: Player[], artifact: ArtifactCard) => {
   if(artifact.isObtainable) return;
-  if(playerOwnsArtifact(currentPlayer, artifact)) return;
+  if(playerOwnsArtifact(currentPlayer, artifact.id)) return;
   
   let artifactOwner!: Player;   
   players.forEach((player) => {
-    if(playerOwnsArtifact(player, artifact)) artifactOwner = player;
+    if(playerOwnsArtifact(player, artifact.id)) artifactOwner = player;
   });
 
-  const artifactsOwnedByOwner = ARTIFACT_CARDS_IDS.filter(arctifact => artifactOwner.playerCards.includes(arctifact));
+  const artifactsOwnedByOwner = ARTIFACT_CARDS_IDS.filter(arctifact => playerOwnsArtifact(artifactOwner, arctifact));
   const tax = artifact.tax[artifactsOwnedByOwner.length-1];
 
   currentPlayer.giveMoney(tax);
   artifactOwner.takeMoney(tax);
 }
 
-const playerOwnsArtifact = (player: Player, artifact: ArtifactCard): boolean => {
-  return player.playerCards.includes(artifact.id)
+const playerOwnsArtifact = (player: Player, artifactId: number): boolean => {
+  return player.playerCards.includes(artifactId)
 }
