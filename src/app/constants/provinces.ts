821b1eb6c9eda7aefcc8a8a1d384ab3f753a6639
marlexxa <marlexxa@gmail.com>
2021-02-13 01:04:25 +0100

    feat(provinces): add ids of each province as constants

--- src/app/constants/provinces.ts
+++ src/app/constants/provinces.ts
@@ -8,3 +8,12 @@ export enum Province {
   GONDOR,
   MORDOR
 }
+
+export const SHIRE_IDS = [1, 4];
+export const LONE_LANDS_IDS = [6, 7, 9];
+export const ERIADOR_IDS = [11, 12];
+export const HITHAEGLIR_IDS = [16, 17, 19];
+export const ROHAN_IDS = [21, 22, 24];
+export const CALENARDHON_IDS = [26, 27];
+export const GONDOR_IDS = [31, 32, 34];
+export const MORDOR_IDS = [39];
