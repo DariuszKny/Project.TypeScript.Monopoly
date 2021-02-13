e0a236006412ec53db84069f6e6c4ca7df565ce9
marlexxa <marlexxa@gmail.com>
2021-02-13 22:07:42 +0100

    refactor: add card import statements

---
+++ src/app/services/game-board.service.ts
@@ -0,0 +1,9 @@
+import { Card } from "../models/card.models/abstract-card.model";
+import { BaseCard } from "../models/card.models/base-card.model";
+import { CityCard } from "../models/card.models/city-card.model";
+import { TrapCard } from "../models/card.models/trap-card.model";
+import { FateCard } from "../models/card.models/fate-card.model";
+import { ArtifactCard } from "../models/card.models/artifact-card.model";
+import { HobbitCard } from "../models/card.models/hobbit-card.model";
+
+
