5f885b3588c4242c5a241d2d38a38b538529a1a4
marlexxa <marlexxa@gmail.com>
2021-02-13 20:28:35 +0100

    feat(player): improve move method
    Now player waits 2 turn if is in jail and gets money for passing START
    field

--- src/app/services/card.services/base-card.service.ts
+++ src/app/services/card.services/base-card.service.ts
@@ -1,12 +1,16 @@
 import { Player } from "../../models/player.model";
 import { JAIL_FIELD } from "../../constants/game-constants";
+import { PASSING_START_SALARY } from "../../constants/prices";
 
-export const playerPassedStart = (player: Player): boolean => {
-  if (player.playerIsJailed) return false;
-  return player.playerPreviousPosition > player.playerCurrentPosition;
+export const playerPassedStart = (player: Player): void => {
+  if (player.playerIsJailed) return;
+  if(player.playerPreviousPosition > player.playerCurrentPosition) {
+    player.takeMoney(PASSING_START_SALARY);
+  }
 }
 
 export const goToJail = (player: Player): void => {
   player.playerIsJailed = true;
+  player.playerBlockedTurns = 2;
   player.playerCurrentPosition = JAIL_FIELD;
 }
