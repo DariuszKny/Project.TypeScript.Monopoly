98af1d162739f8c68d7f50b386b11746d116008b
marlexxa <marlexxa@gmail.com>
2021-02-13 18:37:02 +0100

    feat(constants): add HOBBIT_CARDS_IDS array

--- src/app/constants/game-constants.ts
+++ src/app/constants/game-constants.ts
@@ -2,4 +2,5 @@ export const NUMBER_OF_FIELDS = 40;
 export const JAIL_FIELD = 10;
 export const PLAYER_IS_CHASED_STEPS = 1;
 export const PLAYER_ATTACKED_STEPS = -4;
-export const ARTIFACT_CARDS_IDS = [8, 18, 28, 38]
+export const ARTIFACT_CARDS_IDS = [8, 18, 28, 38];
+export const HOBBIT_CARDS_IDS = [5, 15, 25, 35];
