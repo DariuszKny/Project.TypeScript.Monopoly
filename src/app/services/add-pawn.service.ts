6b7dc84ae4490ff43e79a111388d84b4988027fe
marlexxa <marlexxa@gmail.com>
2021-02-15 00:16:58 +0100

    refactor: move function responsible for moving pawns on board

--- src/app/services/add-pawn.service.ts
+++ src/app/services/add-pawn.service.ts
@@ -1,3 +1,5 @@
+import { Player } from "../models/player.model";
+
 export function addPawn(index: number) {
 
     enum colors {BLUE, GREEN, YELLOW, RED};
@@ -10,4 +12,21 @@ export function addPawn(index: number) {
     newPawn.setAttribute('id', colors[index]);
     nextField.appendChild(newPawn)
 
+}
+
+export const movePawnOnBoard = function (player: Player) {
+  const positonOnBoard: number = player.playerCurrentPosition;
+  const images = require('../../../images/pawns/*.png');
+  const nextField = document.querySelector(`#f${positonOnBoard} > .pawns-container `)!;
+  const pawn = document.querySelector<HTMLElement>(`#${player.color}`)!;
+  const newPawn = document.createElement('img');
+
+  pawn.parentNode!.removeChild(pawn);
+  newPawn.src = images[player.color];
+  newPawn.setAttribute('id', player.color);
+  if(nextField) {
+    nextField.appendChild(newPawn)
+  } else {
+    console.log("NOT FOUND on field "+ positonOnBoard)
+  }
 }
