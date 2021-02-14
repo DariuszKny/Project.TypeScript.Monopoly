edf5324c69fe7afe30fbcaa69c3de56c5ebb42df
marlexxa <marlexxa@gmail.com>
2021-02-14 20:36:37 +0100

    refactor(constants): change constant name

--- src/app/services/card.services/trap-card.service.ts
+++ src/app/services/card.services/trap-card.service.ts
@@ -3,14 +3,14 @@ import { playerLosesMoney } from "./shared-card.service";
 import { Messages } from "../messasges.service";
 import { logMessage } from "../info-board.service";
 import { TRAP_TAX } from "../../constants/prices";
-import { PLAYER_ATTACKED_STEPS, PLAYER_IS_CHASED_STEPS } from "../../constants/game-constants";
+import { PLAYER_IS_ATTACKED_STEPS, PLAYER_IS_CHASED_STEPS } from "../../constants/game-constants";
 
 export const loseMoneyAndMoveBackward = (player: Player): void => {
   loseMoneyAndMove(player, TRAP_TAX, PLAYER_IS_CHASED_STEPS);
 }
 
 export const loseMoneyAndMoveForward = (player: Player): void => {
-  loseMoneyAndMove(player, TRAP_TAX, PLAYER_ATTACKED_STEPS);
+  loseMoneyAndMove(player, TRAP_TAX, PLAYER_IS_ATTACKED_STEPS);
 }
 
 const loseMoneyAndMove = (player: Player, money: number, fieldsToMove: number): void => {
