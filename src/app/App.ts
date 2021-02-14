26d1266262ef6e05bdcc9bed2e51229c954a45a9
marlexxa <marlexxa@gmail.com>
2021-02-14 21:00:30 +0100

    refactor(App): delete unused import statements

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,11 +1,4 @@
-import { doubleCheck } from "./controllers/double-check.controller";
-import { movePlayer } from "./controllers/move.controller";
-import { newRound } from "./controllers/new-round.controller";
-import { throwDice } from "./models/dice.model";
-import { Player } from "./models/player.model";
-import { showThrowResults } from "./services/info-board.service";
 import {GameController} from "./controllers/game.controller"
-import { addPawn } from "./services/add-pawn.service";
 
 class App {
     _init() {
