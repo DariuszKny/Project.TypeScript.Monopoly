79f8e7df5ea7f3dd2e58acbfc7df42da8d30631c
DariuszKny <nasz_email_do_github>
2021-02-14 20:20:29 +0100

    refractor(refractor): refractor code to be more clrea to read and gameControlls be connected to the model

---
+++ src/app/models/game.model.ts
@@ -0,0 +1,45 @@
+import {Player} from "./player.model";
+import {gameBoard, gameField} from "../services/game-board.service"
+import {PlayerService} from "../services/player.service"
+
+export class GameModel {
+    private _players: Player[] =  PlayerService.createPlayers();
+    private _activePlayer: Player = this._players[0];
+    private _gameBoard = gameBoard;
+    private _isEnded = false;
+    readonly timeToEnd: number;
+
+    constructor(timeToEnd: number) {
+        this.timeToEnd = timeToEnd;
+    }
+
+
+    get players(): Player[] {
+        return this._players;
+    }
+
+    set players(value: Player[]) {
+        this._players = value;
+    }
+
+    get activePlayer(): Player {
+        return this._activePlayer;
+    }
+
+    set activePlayer(value: Player) {
+        this._activePlayer = value;
+    }
+
+    get gameBoard(): gameField[] {
+        return this._gameBoard;
+    }
+
+
+    get isEnded(): boolean {
+        return this._isEnded;
+    }
+
+    set isEnded(value: boolean) {
+        this._isEnded = value;
+    }
+}
