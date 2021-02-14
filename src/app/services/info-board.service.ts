9dd6255c6f5621288efb4717998040e3f8c83c29
marlexxa <marlexxa@gmail.com>
2021-02-14 18:37:09 +0100

    refactor(info-board): refactor showThrowResults() body

--- src/app/services/info-board.service.ts
+++ src/app/services/info-board.service.ts
@@ -4,17 +4,13 @@ import { playerRolledDiceMessage } from "./messasges.service";
 const images = require("../../../images/dice/*.png");
 
 export function showThrowResults(dice: number[], currentPlayer: Player) {
-
     const die1 = document.querySelector<HTMLImageElement>("#die1")!;
     const die2 = document.querySelector<HTMLImageElement>("#die2")!;
-    const info = document.querySelector<HTMLElement>(".message-container")!;
 
     die1.src = images[`DICEROLL${dice[0]}`];
     die2.src = images[`DICEROLL${dice[1]}`];
 
-    const p = document.createElement("p");
-    const node = document.createTextNode(`${currentPlayer.playerName} has rolled ${dice[0]} and ${dice[1]}`)
-    p.appendChild(node);
+    logMessage(playerRolledDiceMessage(currentPlayer, dice));
 }
 
 export const logMessage = (message: string): void => {
