43d7088c85495c0668e094708e5d6dbf4a42baf9
marlexxa <marlexxa@gmail.com>
2021-02-15 02:08:03 +0100

    fix: asign zero when not double

--- src/app/controllers/double-check.controller.ts
+++ src/app/controllers/double-check.controller.ts
@@ -3,9 +3,9 @@ import { Player } from "../models/player.model";
 export function doubleCheck(throwD: number[], currentPlayer: Player) {
     if (throwD[0] === throwD[1]) {
         currentPlayer.playerDouble++;
-
         if (currentPlayer.playerDouble !== 3) {
             currentPlayer.playerAreDiceRolled = false;
         }
     }
+    else currentPlayer.playerDouble = 0;
 }
